a10500ac55a0ad6d100456adb543a536e681d8a8
Yeasin <realalyeasin@gmail.com>
2022-03-25 20:52:39 +0600

    operation

---
+++ BlazorOperation/wwwroot/js/common.js
@@ -0,0 +1,21 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successfull", { timeout: 5000 });
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failed", { timeout: 5000 });
+    }
+}
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        swal({
+            title: "Here's a title!",
+        });
+    }
+    if (type === "error") {
+        swal({
+            title: "Here's a title!",
+        });
+    }
+}
